0ab96344e1892895347d49126b071c7305652fbd
Nathan Watts <nathan@naykel.com.au>
2024-04-05 05:41:25 +1000

    install contactit

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,11 @@
+<x-gt-app-layout layout="{{ config('naykel.template') }}" class="py-5-3-2-2" :$pageTitle>
+    <div class="container-sm">
+        <div class="bx light">
+            <div class="tac mb-2">
+                <h2 class="mx-3">Have Questions?</h2>
+                <p class="lead">Fill out the form below and we'll be in touch soon.</p>
+            </div>
+            <livewire:contact />
+        </div>
+    </div>
+</x-gt-app-layout>
